1c25dde0fa8e89a057a9e73ef63d355e49ea8a2d
Kunal Shewale <kunal.shewale@synerzip.com>
2013-10-23 01:12:05 +0530

    Created a tutorial to understand ANGULAR's working.

--- app/app.js
+++ app/app.js
@@ -2,3 +2,5 @@
  * Created by kunal on 22/10/13.
  * Main or entry level file for the application. Application will initiate from this file.
  */
+
+var app = angular.module('TVPremieresApp',[]);
--- app/filters/filters.js
+++ app/filters/filters.js
@@ -2,3 +2,21 @@
  * Created by kunal on 22/10/13.
  * Filters for the application will reside here
  */
+
+app.filter('isGenre', function() {
+    return function(input, genre) {
+        if (typeof genre == 'undefined' || genre == null) {
+            return input;
+        } else {
+            var out = [];
+            for (var a = 0; a < input.length; a++){
+                for (var b = 0; b < input[a].show.genres.length; b++){
+                    if(input[a].show.genres[b] == genre) {
+                        out.push(input[a]);
+                    }
+                }
+            }
+            return out;
+        }
+    };
+});
